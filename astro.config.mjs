60643f5ecce9adff6282a4d82fae66b7fb49e8db
swadhin biswas <swadhinbiswas.cse@gmail.com>
2025-09-30 03:06:31 +0600

    Refactore : Astro Stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cloudflare from '@astrojs/cloudflare';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
+});
